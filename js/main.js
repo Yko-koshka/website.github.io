2709ca146d6aca3f95620131e6d959c4618a8ecb
Hanna <legezaanna7@gmail.com>
2020-12-18 21:01:46 +0200

    adding a slider and style

--- js/main.js
+++ js/main.js
@@ -1,3 +1,30 @@
 $(function(){
-    alert('ready');
+    $('.slider__inner').slick(
+        {
+            // dots: true,
+            infinite: true,
+            adaptiveHeight: true,
+            autoplay: true,
+            autoplaySpeed: 2000,
+            fade: true,
+            cssEase: 'linear',
+            arrows: false,
+            // responsive: [
+            //     {
+            //         breakpoint: 1024,
+            //         settings: {
+            //         slidesToShow: 3,
+            //         slidesToScroll: 3,
+            //         infinite: true,
+            //         dots: true
+            //         }
+            //     }
+            // ]
+        }
+    );
 });
+// $(document).ready(function(){
+//     $('.your-class').slick({
+//       setting-name: setting-value
+//     });
+//   });
