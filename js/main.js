b8d89653b29494434fd9d19b97ab778b3b0fe33e
Hanna <legezaanna7@gmail.com>
2021-01-21 18:03:21 +0200

    new styles added

--- js/main.js
+++ js/main.js
@@ -1,27 +1,15 @@
 $(function(){
-    $('.slider__inner').slick(
-        {
-            // dots: true,
-            infinite: true,
-            adaptiveHeight: true,
-            autoplay: true,
-            autoplaySpeed: 2000,
-            fade: true,
-            cssEase: 'linear',
-            arrows: false,
-            // responsive: [
-            //     {
-            //         breakpoint: 1024,
-            //         settings: {
-            //         slidesToShow: 3,
-            //         slidesToScroll: 3,
-            //         infinite: true,
-            //         dots: true
-            //         }
-            //     }
-            // ]
-        }
-    );
+    $('.slider__inner').slick({
+        infinite: true,
+        speed: 300,
+        slidesToShow: 1,
+        centerMode: true,
+        variableWidth: true,
+        adaptiveHeight: true,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        arrows: false,
+    });
 });
 // $(document).ready(function(){
 //     $('.your-class').slick({
