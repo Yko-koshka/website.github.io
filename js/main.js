e46c92a508dda064978342d6bd26e8658417811c
Hanna <legezaanna7@gmail.com>
2021-07-11 21:09:29 +0300

    animations added

--- js/main.js
+++ js/main.js
@@ -69,3 +69,35 @@ window.onload = ()=>{
 function videoUrl(item) {
   document.getElementById("slider-video").src = item;
 }
+
+//tabs
+const tabsBtn   = document.querySelectorAll(".tabs__btn");
+const tabsItems = document.querySelectorAll(".tabs__item");
+
+tabsBtn.forEach(onTabClick);
+
+function onTabClick(item) {
+    item.addEventListener("click", function() {
+        let currentBtn = item;
+        let tabId = currentBtn.getAttribute("data-tab");
+        let currentTab = document.querySelector(tabId);
+
+        if( ! currentBtn.classList.contains('active') ) {
+            tabsBtn.forEach(function(item) {
+                item.classList.remove('active');
+            });
+    
+            tabsItems.forEach(function(item) {
+                item.classList.remove('active');
+            });
+    
+            currentBtn.classList.add('active');
+            currentTab.classList.add('active');
+        }
+    });
+}
+
+document.querySelector('.tabs__btn').click();
+
+// WOW
+new WOW().init();
